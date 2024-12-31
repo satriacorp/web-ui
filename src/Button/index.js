78840635bf07df7f74b8e3e64cb945386aa876e6
satriawanbasri <satriawan.basri@gmail.com>
2024-12-31 23:07:00 +0800

    -

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,7 +1,36 @@
-import React, { useContext } from 'react'
+import { useContext, useState } from 'react'
 import ThemeContext from '../Contexts/ThemeContext'
+import { getStyles } from './styles'
+import { validColors, validButtonVariants } from '@satriacorp/utils'
 
-export default ({ label }) => {
+export default ({ variant, color, disabled, children, onClick, style }) => {
+    variant = validButtonVariants.includes(variant) ? variant : validButtonVariants[0]
+    color = validColors.includes(color) ? color : validColors[0]
+
     const { theme } = useContext(ThemeContext)
-    return <button>{label + ' - ' + theme}</button>
+    const styles = getStyles(theme, color)
+
+    const [isActive, setIsActive] = useState(false)
+    const [isHover, setIsHover] = useState(false)
+
+    const baseStyle = styles.base
+    const variantStyle = styles[variant].base || styles.contained.base
+    const hoverStyle = isHover ? styles[variant].hover : null
+    const activeStyle = isActive ? styles[variant].active : null
+    const disabledStyle = disabled ? styles[variant].disabled : null
+
+    const combinedStyle = { ...baseStyle, ...variantStyle, ...hoverStyle, ...activeStyle, ...disabledStyle, ...style }
+
+    return (
+        <button
+            style={combinedStyle}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            onMouseDown={() => setIsActive(true)}
+            onMouseUp={() => setIsActive(false)}
+            disabled={disabled}
+            onClick={onClick}>
+            {children}
+        </button>
+    )
 }
--- src/Button/styles.js
+++ src/Button/styles.js
@@ -1,1 +1,78 @@
-
+export const getStyles = (theme, color) => {
+    return {
+        base: {
+            ...theme.typography.button,
+            paddingTop: 8,
+            paddingBottom: 8,
+            minWidth: 120,
+            borderRadius: theme.shape.borderRadius,
+            cursor: 'pointer',
+            textAlign: 'center',
+            transition: 'all 0.1s ease-in-out',
+            transform: 'translateY(-2px)',
+        },
+        contained: {
+            base: {
+                border: 'none',
+                backgroundColor: theme.palette[color].main,
+                color: theme.palette[color].contrastText,
+                boxShadow: theme.shadows[4],
+            },
+            hover: {
+                backgroundColor: theme.palette[color].light,
+            },
+            active: {
+                transform: 'translateY(0)',
+                boxShadow: theme.shadows[0],
+            },
+            disabled: {
+                cursor: 'not-allowed',
+                boxShadow: theme.shadows[0],
+                backgroundColor: theme.palette[color].dark,
+            },
+        },
+        outlined: {
+            base: {
+                borderStyle: 'solid',
+                borderWidth: 1,
+                borderColor: theme.palette[color].main,
+                backgroundColor: 'transparent',
+                color: theme.palette[color].main,
+                boxShadow: theme.shadows[4],
+            },
+            hover: {
+                borderColor: theme.palette[color].light,
+                color: theme.palette[color].light,
+            },
+            active: {
+                transform: 'translateY(0)',
+                boxShadow: theme.shadows[0],
+            },
+            disabled: {
+                cursor: 'not-allowed',
+                boxShadow: theme.shadows[0],
+                borderColor: theme.palette[color].dark,
+                color: theme.palette[color].dark,
+            },
+        },
+        text: {
+            base: {
+                border: 'none',
+                backgroundColor: 'transparent',
+                color: theme.palette[color].main,
+                boxShadow: theme.shadows[0],
+            },
+            hover: {
+                backgroundColor: theme.palette[color].light,
+                color: theme.palette[color].contrastText,
+            },
+            active: {
+                transform: 'translateY(0)',
+            },
+            disabled: {
+                cursor: 'not-allowed',
+                color: theme.palette[color].dark,
+            },
+        },
+    }
+}
